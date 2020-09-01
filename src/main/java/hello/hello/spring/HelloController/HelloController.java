7d38c42e14a7224933e592739dabb7a958f2489b
TAEIL JUNG <taeil@naver.com>
2020-09-01 23:00:27 +0900

    hello-static, hello-template파일 추가 HelloController 수정: hello-mvc 맵핑추가

--- src/main/java/hello/hello/spring/HelloController/HelloController.java
+++ src/main/java/hello/hello/spring/HelloController/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello.spring.HelloController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import sun.font.EAttribute;
 
 @Controller
@@ -13,5 +14,12 @@ public class HelloController {
 
         model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name")String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+
     }
 }
